0102c8a0f11ede1a432354053b15fdc65c97d66f
kapuska <44909493+kapuskaFaizan@users.noreply.github.com>
2020-11-24 16:59:46 +0530

    Update and rename app_script.py to API_recommender_system.py

--- API_recommender_system.py
+++ API_recommender_system.py
@@ -1,32 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#importing all dependencies
 from func.classes_and_functions import Read_train
-
-
-# In[2]:
-
+from flask import Flask,request
+import pandas as pd
 
 n_users,n_posts,train,test= Read_train.read_data('self')
 trainn,testt = Read_train.user_post_ids('self')
 model = Read_train.get_model()
 
-
-# In[4]:
-
-
 data =Read_train.get_estimation_data()
 
-
-# In[ ]:
-
-
-from flask import Flask,request
-import pandas as pd
 app = Flask(__name__)
  
 @app.route("/",methods =['GET','POST'])
@@ -42,10 +23,3 @@ def select():
  
 if __name__ == "__main__":
     app.run()
-
-
-# In[ ]:
-
-
-
-
